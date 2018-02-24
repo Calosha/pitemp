c5714d77ad8ced82781847e4d745252cbc4b1905
Alexey Chernyuk <alexey.chernyuk@gmail.com>
2018-02-24 15:40:19 -0500

    fix error
    fix error

--- web_root/js/test_call_py.php
+++ web_root/js/test_call_py.php
@@ -1,5 +1,5 @@
 <?php 
-bool set_time_limit (30);
+set_time_limit (30);
 $command = escapeshellcmd('../../ext/Adafruit_Python_DHT/examples/AdafruitDHT.py');
 $output = shell_exec($command);
 echo $output;
