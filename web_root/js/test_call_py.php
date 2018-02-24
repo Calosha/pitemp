0f524a31157624d8beb258e8ae5b2acb6d419cc7
Alexey Chernyuk <alexey.chernyuk@gmail.com>
2018-02-24 16:05:02 -0500

    trying exec
    trying exec

--- web_root/js/test_call_py.php
+++ web_root/js/test_call_py.php
@@ -2,8 +2,7 @@
 set_time_limit (30);
 $command = escapeshellcmd('../../ext/Adafruit_Python_DHT/examples/AdafruitDHT.py');
 var_dump("executing");
-$output = shell_exec($command);
-sleep (10);
+$output = exec($command);
 var_dump('executed');
 var_dump($output) ;
 
