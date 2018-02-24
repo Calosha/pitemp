7a89cced818243ec753312eb3d921df566e3d140
Alexey Chernyuk <alexey.chernyuk@gmail.com>
2018-02-24 15:43:52 -0500

    test
    test

--- ext/Adafruit_Python_DHT/examples/AdafruitDHT.py
+++ ext/Adafruit_Python_DHT/examples/AdafruitDHT.py
@@ -32,6 +32,7 @@ pin = 23
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+print(temperature)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
