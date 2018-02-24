ed816a0f8ac7446c317be941eb7ab261afcd12e5
Alexey Chernyuk <alexey.chernyuk@gmail.com>
2018-02-24 15:52:25 -0500

    test
    test

--- web_root/js/test_call_py.php
+++ web_root/js/test_call_py.php
@@ -3,7 +3,7 @@ set_time_limit (30);
 $command = escapeshellcmd('../../ext/Adafruit_Python_DHT/examples/AdafruitDHT.py');
 var_dump("executing");
 $output = shell_exec($command);
-int sleep (10);
+sleep (10);
 var_dump('executed');
 var_dump($output) ;
 
